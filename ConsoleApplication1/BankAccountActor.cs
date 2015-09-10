2acbf88c83ea6316dbc9086317eff9ff12abd375
Piet Cordemans <piet.cordemans@kuleuven.be>
2015-09-10 12:06:11 +0200

    example with akka.NET and vs unit test

---
+++ ConsoleApplication1/BankAccountActor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Akka;
+using Akka.Actor;
+
+namespace AkkaExperiments
+{
+    public class Transfer
+    {
+        /// <summary>
+        /// Transfer message to send to a BankAccountActor
+        /// </summary>
+        /// <param name="amount">amount to send</param>
+        /// <param name="dest">destination BankAccountActor</param>
+        public Transfer(int amount, IActorRef dest)
+        {
+            this.amount = amount;
+            this.dest = dest;
+        }
+        public int amount { get; private set; }
+        public IActorRef dest { get; private set; }
+    }
+    /// <summary>
+    /// Balance message to send to a BankAccountActor
+    /// </summary>
+    public class Balance  {}
+
+    public class Deposit
+    {
+        /// <summary>
+        /// Deposit message to send to a BankAccountActor
+        /// </summary>
+        /// <param name="amount">amount to sent</param>
+        public Deposit(int amount)
+        {
+            this.amount = amount;
+        }
+        public int amount { get; private set; }
+    }
+    
+    /// <summary>
+    /// Actor representing a bankaccount
+    /// </summary>
+    public class BankAccountActor : ReceiveActor
+    {
+        private int balance;
+
+        /// <summary>
+        /// Initializes a bankaccount with a balance of 1000
+        /// Registers behavior when receiving a message
+        /// </summary>
+        public BankAccountActor() {
+            balance = 1000;
+            
+            Receive<Deposit>(msg => balance += msg.amount);
+            Receive<Balance>(msg => Sender.Tell(balance));
+            Receive<Transfer>(msg => transfer(msg));
+        }
+
+        private void transfer(Transfer msg)
+        {
+            balance -= msg.amount;
+            msg.dest.Tell(new Deposit(msg.amount));
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Akka;
+using Akka.Actor;
+using System.Threading.Tasks;
+
+
+namespace AkkaExperiments
+{
+    [TestClass]
+    public class AkkaTest
+    {
+        ActorSystem system;
+        IActorRef src;
+        IActorRef dst;
+
+        /// <summary>
+        /// Setup the actor system and create two actors (supervised by the system)
+        /// </summary>
+        [TestInitialize]
+        public void setup()
+        {
+            system = ActorSystem.Create("MySystem");
+            src = system.ActorOf<BankAccountActor>("src");
+            dst = system.ActorOf<BankAccountActor>("dst");
+
+        }
+
+        /// <summary>
+        /// Send a transfer message to the src actor for 500 to dst
+        /// Concurrently ask the destination its current balance
+        /// Both messages are in a race
+        /// </summary>
+        [TestMethod]
+        public void testBankActorNonDeterministic()
+        {
+            src.Tell(new Transfer(500, dst));
+
+            //Change the delay (1 .. 1000) in order to observe the different behavior (not guaranteed)
+            var t = Task.Delay(1000);
+            t.Wait();
+
+                      
+            var t2 = dst.Ask(new Balance());
+
+            
+            Assert.AreEqual(1500, t2.Result);
+        }        
+    }
+}
